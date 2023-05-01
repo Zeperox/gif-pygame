4e6304637aa8e54f1476fe3bbb3dfccb04a24e25
Zeperox <53876273+Zeperox@users.noreply.github.com>
2023-05-01 05:36:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as readme:
 
 setup(
     name="gif_pygame",
-    version="0.0.5",
+    version="0.0.6",
     author="Zeperox",
     description="A pygame addon for animated image files",
     long_description=LONG_DESCRIPTION,
