d688377d6753e561aef0125742ddf85dfa3b24e0
Zeperox <53876273+Zeperox@users.noreply.github.com>
2026-04-24 14:44:49 +0800

    1.2.4
    fixed typo

--- gif_pygame/gif_pygame.py
+++ gif_pygame/gif_pygame.py
@@ -17,7 +17,7 @@ A tool to make loading animated image files in pygame easier
 	>>> More in docs
 """
 
-version = "1.2.3"
+version = "1.2.4"
 
 import pygame, time, warnings
 
--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
 from setuptools import setup, find_packages
 
 with open('README.md', encoding='utf-8') as readme:
-    LONG_DESCRIPTION = readme.read()
+	LONG_DESCRIPTION = readme.read()
 
 setup(
-    name="gif_pygame",
-    version="1.2.3",
-    author="Zeperox",
-    description="A pygame addon for animated image files",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=["pygame-ce>=2.5.4", "pillow>=10.0.0", "packaging>=20.7"],
-    keywords=["python", "pygame", "addon", "image", "animation", "animated images"],
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: pygame",
-        "Operating System :: OS Independent"
-    ],
-    python_requires=">=3.7"
+	name="gif_pygame",
+	version="1.2.4",
+	author="Zeperox",
+	description="A pygame addon for animated image files",
+	long_description=LONG_DESCRIPTION,
+	long_description_content_type="text/markdown",
+	packages=find_packages(),
+	install_requires=["pygame-ce>=2.5.4", "pillow>=10.0.0", "packaging>=20.7"],
+	keywords=["python", "pygame", "addon", "image", "animation", "animated images"],
+	classifiers=[
+		"Development Status :: 6 - Mature",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
+		"Programming Language :: Python :: 3.13",
+		"Programming Language :: Python :: 3.14",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy",
+		"Topic :: Software Development :: Libraries :: pygame",
+		"Operating System :: OS Independent"
+	],
+	python_requires=">=3.7"
 )
